94dfc6d4ca5971d355c4f5fe4482478d375166ff
dophire <38838633+dophire@users.noreply.github.com>
2026-01-19 01:36:05 -0600

    By Vudgekek (Fix Optifine Custom Sky) Remove Custom Resource Pack Repository
    This fix was made by @Vudgekek

--- src/main/java/chylex/respack/ResourcePackOrganizer.java
+++ src/main/java/chylex/respack/ResourcePackOrganizer.java
@@ -18,7 +18,6 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import chylex.respack.ConfigHandler.DisplayPosition;
 import chylex.respack.gui.GuiCustomResourcePacks;
 import chylex.respack.render.RenderPackListOverlay;
-import chylex.respack.repository.ResourcePackRepositoryCustom;
 
 @Mod(modid = ResourcePackOrganizer.MODID, name = ResourcePackOrganizer.MODNAME, useMetadata = true, guiFactory = "chylex.respack.gui.GuiModConfig")
 public final class ResourcePackOrganizer{
@@ -43,7 +42,6 @@ public final class ResourcePackOrganizer{
 		
 		MinecraftForge.EVENT_BUS.register(this);
 		
-		ResourcePackRepositoryCustom.overrideRepository(config.options.getEnabledPacks());
 		RenderPackListOverlay.refreshPackNames();
 	}
 	
